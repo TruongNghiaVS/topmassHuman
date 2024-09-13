15734675a6ea5d4b8950a117b05f84be3b5223d8
thai.nn <thai.nn@tinhhoa.asia>
2024-09-13 17:55:02 +0700

    update small code

--- src/app/hr-center/chien-dich-tuyen-dung/tao-tin-dang/setting/time-working-form.tsx
+++ src/app/hr-center/chien-dich-tuyen-dung/tao-tin-dang/setting/time-working-form.tsx
@@ -1,9 +1,9 @@
 import { useFieldArray } from "react-hook-form";
-import { ILocationForm } from "./location-form";
 import TmInput from "@/component/hook-form/input";
 import { ClockIcon, PlusIcon, TrashIcon } from "@heroicons/react/16/solid";
 import { dayOfWeek } from "@/mockup-data/data";
 import TmSelect from "@/component/hook-form/select";
+import { ILocationForm } from "@/interface/interface";
 
 export const TimeWorkingForm = ({ control, name }: ILocationForm) => {
   const { fields, append, remove } = useFieldArray({
